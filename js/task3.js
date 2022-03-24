4f037c25ed807bcd1b3865d67014750995cd0da9
agha-safarzada <agaseferzade2003@gmail.com>
2022-03-24 13:28:10 +0400

    task3 commit

---
+++ js/task3.js
@@ -0,0 +1,11 @@
+function evenNum(array) {
+    let even = [];
+    for (i = 0; i < array.length; i++) {
+        if (array[i] % 2 == 0) {
+            even.push(array[i]);
+        }
+    }
+    console.log(even);
+}
+
+evenNum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
